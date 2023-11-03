225c995406d5a9322f5c738a22385b9b1123d373
llenny18 <ramosalleneid01@gmail.com>
2023-11-03 16:12:29 +0800

    improvements

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -2,14 +2,17 @@ import express from "express";
 const router = express.Router();
 import database from "../db/connection.mjs";
 import sha256 from "sha256";
+import Swal from "sweetalert2";
+
 
 
 
 router.get("/",(req,res)=>{
     res.render('./admin-moderator/index',{
+        
         usertype: "Administrator" //DON'T REMOVE
     });
-   
+    
 });
 
 router.get("/dashboard", (req,res)=>{
@@ -70,45 +73,50 @@ router.post('/login', function(request, response, next){
     var user_password = request.body.user_password;
     console.log(user_email_address + " = " + user_password);
 
-    if(user_email_address && user_password)
+    if(!user_email_address && !user_password)
     {
+        response.send('Please Enter Email Address and Password Details');
+        response.end();
+    }
+    else
+    {
+       
         var query = `
-        SELECT * FROM superusers 
-        WHERE userName = "${user_email_address}"
+        SELECT superID,uPassword FROM superusers 
+        WHERE userName = ?
         `;
         console.log(query);
 
-        database.query(query, function(error, data){
+        database.query(query, [user_email_address],function(error, data){
 
-            if(data.length > 0)
+            if(data.length == 0)
             {
+                response.send('Incorrect Email Address');
+            }
+            else
+            {
+               
                 for(var count = 0; count < data.length; count++)
                 {
                     let valPassword = sha256(user_password)
-                    if(data[count].uPassword == valPassword)
+                    if(data[count].uPassword != valPassword)
+                    {
+                        
+                        response.send("<script>alert('Wrong Password!'); window.location.replace('/admin');</script> ");
+
+                    }
+                    else
                     {
                         
                         request.session.superID = data[count].superID;
 
-                        response.redirect("/admin/dashboard");
-                    }
-                    else
-                    {
-                        response.send('Incorrect Password');
+                        response.redirect("dashboard");
                     }
                 }
-            }
-            else
-            {
-                response.send('Incorrect Email Address');
+
             }
             response.end();
         });
-    }
-    else
-    {
-        response.send('Please Enter Email Address and Password Details');
-        response.end();
     }
 
 });
