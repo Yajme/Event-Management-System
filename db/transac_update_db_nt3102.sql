e0b45495d1cbdfb0eb54816f882a4449a590bf01
Aleister Alinsunurin <115854134+AliHaruto@users.noreply.github.com>
2023-11-17 15:16:34 +0800

    Add files via upload

---
+++ db/transac_update_db_nt3102.sql
@@ -0,0 +1,309 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 17, 2023 at 07:15 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_nt3102`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE IF NOT EXISTS `course` (
+  `courseID` int NOT NULL AUTO_INCREMENT,
+  `courseName` text NOT NULL,
+  `dept_ID` int NOT NULL,
+  PRIMARY KEY (`courseID`),
+  KEY `dept_ID` (`dept_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`courseID`, `courseName`, `dept_ID`) VALUES
+(1, 'Bachelor of Science in Business Administration', 1),
+(2, 'Bachelor of Science in Management Accounting', 1),
+(3, 'Bachelor of Science in Psychology', 2),
+(4, 'Bachelor of Arts in Communication', 2),
+(5, 'Bachelor of Industrial Technology', 3),
+(6, 'Bachelor of Science in Information Technology', 4),
+(7, 'Bachelor of Science in Computer Science', 4),
+(8, 'Bachelor of Secondary Education', 5),
+(9, 'Bachelor of Science in Industrial Engineering ', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE IF NOT EXISTS `department` (
+  `dept_ID` int NOT NULL AUTO_INCREMENT,
+  `department_Name` varchar(100) NOT NULL,
+  PRIMARY KEY (`dept_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`dept_ID`, `department_Name`) VALUES
+(1, 'CABEIHM'),
+(2, 'CAS'),
+(3, 'CIT'),
+(4, 'CICS'),
+(5, 'CTE'),
+(6, 'CE'),
+(0, 'General Department');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventattendees`
+--
+
+DROP TABLE IF EXISTS `eventattendees`;
+CREATE TABLE IF NOT EXISTS `eventattendees` (
+  `attendeeID` int NOT NULL AUTO_INCREMENT,
+  `eventID` int NOT NULL,
+  `sr_code` int NOT NULL,
+  `DateRegistered` date NOT NULL,
+  PRIMARY KEY (`attendeeID`),
+  KEY `eventID` (`eventID`),
+  KEY `sr_code` (`sr_code`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventrecords`
+--
+
+DROP TABLE IF EXISTS `eventrecords`;
+CREATE TABLE IF NOT EXISTS `eventrecords` (
+  `recordID` int NOT NULL AUTO_INCREMENT,
+  `eventID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `remarks` varchar(255) NOT NULL,
+  `superID` int NOT NULL,
+  PRIMARY KEY (`recordID`),
+  KEY `superID` (`superID`),
+  KEY `statusID` (`eventID`(250)),
+  KEY `fk_eventID` (`eventID`(250))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `eventID` int NOT NULL AUTO_INCREMENT,
+  `eventName` varchar(50) NOT NULL,
+  `eventDesc` varchar(50) NOT NULL,
+  `org_ID` int NOT NULL,
+  `statusID` int NOT NULL,
+  `e_date` datetime NOT NULL,
+  PRIMARY KEY (`eventID`),
+  KEY `org_ID` (`org_ID`),
+  KEY `statusID` (`statusID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventstatus`
+--
+
+DROP TABLE IF EXISTS `eventstatus`;
+CREATE TABLE IF NOT EXISTS `eventstatus` (
+  `statusID` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`statusID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `eventstatus`
+--
+
+INSERT INTO `eventstatus` (`statusID`, `status`) VALUES
+(1, 'Pending'),
+(2, 'Approved'),
+(3, 'Cancelled');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+CREATE TABLE IF NOT EXISTS `organization` (
+  `org_ID` int NOT NULL AUTO_INCREMENT,
+  `dept_ID` int NOT NULL,
+  `org_Name` varchar(255) NOT NULL,
+  `superID` int NOT NULL,
+  PRIMARY KEY (`org_ID`),
+  KEY `superID` (`superID`),
+  KEY `dept_ID` (`dept_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `organization`
+--
+
+INSERT INTO `organization` (`org_ID`, `dept_ID`, `org_Name`, `superID`) VALUES
+(1, 1, 'Junior Philippine Association of Management Accountants', 1),
+(2, 1, 'Junior Marketing Executives', 2),
+(3, 1, 'College of Accountancy, Business and Economics Council', 3),
+(4, 1, 'Public Administration Student Association', 4),
+(5, 1, 'Association Of Operation Management Students', 5),
+(6, 1, 'Young People Management Association of the Philippines', 6),
+(7, 2, 'Association of College of Arts and Sciences Students', 7),
+(8, 2, 'Circle of Psychology Students', 8),
+(9, 2, 'Poderoso Communicador Sociedad', 9),
+(10, 3, 'Alliance of Industrial Technology Students', 10),
+(11, 3, 'CTRL+A', 11),
+(12, 4, 'Junior Philippine Computer Society - Lipa Chapter', 12),
+(13, 4, 'Tech Innovators Society', 13),
+(14, 5, 'Aspiring Future Educators Guild', 14),
+(15, 5, 'Language Educators Association', 15),
+(16, 6, 'Junior Philippine Institute of Industrial Engineers', 16),
+(17, 0, 'Red Spartan Sports Council', 17),
+(18, 0, 'Supreme Student Council Lipa Campus', 18);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
+  `sr_code` varchar(250) NOT NULL,
+  `firstName` varchar(250) NOT NULL,
+  `lastName` varchar(250) NOT NULL,
+  `courseID` int NOT NULL,
+  `year` varchar(255) NOT NULL,
+  `section` varchar(250) NOT NULL,
+  PRIMARY KEY (`sr_code`),
+  KEY `courseID` (`courseID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`sr_code`, `firstName`, `lastName`, `courseID`, `year`, `section`) VALUES
+('21-33470', 'Emjay', 'Rongavilla', 6, '3rd', 'NT-3102'),
+('21-33273', 'Aleister', 'Alinsunurin', 6, '3rd', 'NT-3102');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superusers`
+--
+
+DROP TABLE IF EXISTS `superusers`;
+CREATE TABLE IF NOT EXISTS `superusers` (
+  `superID` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(10) NOT NULL,
+  PRIMARY KEY (`superID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `superusers`
+--
+
+INSERT INTO `superusers` (`superID`, `userName`, `password`, `salt`) VALUES
+(1, 'JPAMA', '3913342ed7e247f86dc7bf83229b90a0cec7d8f7f9a6851927f7becc7fec9035', 'b79dc59cf1'),
+(2, 'JME', '18d7d94a8343f46b943d963da0d1b8bb42520ba84d4329280be02e5c542a9ee4', 'fc39983032'),
+(3, 'CABE Council', '423a577e2d08ee38ad7969840840efd3ebc0adde65ccce896eb93c3d2c491fc8', '02eb527701');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbemployee`
+--
+
+DROP TABLE IF EXISTS `tbemployee`;
+CREATE TABLE IF NOT EXISTS `tbemployee` (
+  `empid` int NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_studentinfo`
+--
+
+DROP TABLE IF EXISTS `tb_studentinfo`;
+CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
+  `studid` int NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_studentinfo`
+--
+
+INSERT INTO `tb_studentinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(1, 'parker', 'peter', 'bsit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userstudents`
+--
+
+DROP TABLE IF EXISTS `userstudents`;
+CREATE TABLE IF NOT EXISTS `userstudents` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `sr_code` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(10) NOT NULL,
+  PRIMARY KEY (`userID`),
+  KEY `sr_code` (`sr_code`(250))
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `userstudents`
+--
+
+INSERT INTO `userstudents` (`userID`, `sr_code`, `password`, `salt`) VALUES
+(1, '21-33470', 'e0999eedf060a2ee05ab267bdb52f827b5f0174d839ac30eae6cd235392531f6', '1ea831d0d9'),
+(3, '21-33273', 'a46509693cff178f34647bb3bdf3b8fffcf36b75e102b9fabeba3417632f7366', '00d8bf7d8b');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
