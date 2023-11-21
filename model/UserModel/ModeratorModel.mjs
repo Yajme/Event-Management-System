9c991c6778ed5267a91f7ac10aa841d1284e646e
Yajme <spartan.emjayrongavilla@gmail.com>
2023-11-21 16:29:23 +0800

    Models for user

---
+++ model/UserModel/ModeratorModel.mjs
@@ -0,0 +1,10 @@
+const Menu = [
+    
+    {route : "dashboard", icon:"mdi mdi-home menu-icon",title:"Home"},
+    {route : "eventlist", icon:"mdi mdi-format-list-checkbox menu-icon",title:"Event List View"},
+    {route : "eventmanagement", icon:"mdi mdi-table-large menu-icon",title:"Event Registration"},
+    {route : "attendlist", icon:"mdi mdi-account-check menu-icon",title:"Student Attendees"},
+
+]
+
+export default Menu;
