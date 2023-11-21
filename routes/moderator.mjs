5d83c85f0351013ced8e3b2f2ef27b5b5b94f077
DzeyEm <jmlaylo7@gmail.com>
2023-11-21 14:17:19 +0800

    commit

--- routes/moderator.mjs
+++ routes/moderator.mjs
@@ -183,6 +183,7 @@ router.post('/login-m', function(request, response, next){
                     {
                         response.cookie("m_std_name", user_email_address, { maxAge: minute }, { httpOnly: true });
                         response.cookie("m_std_id", data[0].superID, { maxAge: minute }, { httpOnly: true });
+                        response.cookie("org_id", data[0].org_ID, { maxAge: minute }, { httpOnly: true });
                         response.cookie("utype", "moderator", { maxAge: minute }, { httpOnly: true });
                         request.session.superID = data[0].superID;
                         response.redirect("/moderator/dashboard");
