da81d6f88e45a32daa67c1a5454e56748b849b4b
Yajme <spartan.emjayrongavilla@gmail.com>
2023-11-25 01:34:42 +0800

    Updated models

--- model/UserModel/ModeratorModel.mjs
+++ model/UserModel/ModeratorModel.mjs
@@ -1,10 +1,58 @@
+import database from '../../db/connection.mjs';
+import EventModel from '../EventModel.mjs';
 const Menu = [
     
     {route : "dashboard", icon:"mdi mdi-home menu-icon",title:"Home"},
     {route : "eventlist", icon:"mdi mdi-format-list-checkbox menu-icon",title:"Event List View"},
-    {route : "eventmanagement", icon:"mdi mdi-table-large menu-icon",title:"Event Registration"},
+    {route : "eventregistration", icon:"mdi mdi-table-large menu-icon",title:"Event Registration"},
     {route : "attendlist", icon:"mdi mdi-account-check menu-icon",title:"Student Attendees"},
 
 ]
 
-export default Menu;
+const superuser = async (username)=> {
+   try{
+    return new Promise((resolve,reject)=>{
+        const query = `SELECT * FROM superusers WHERE userName = ? AND superID != 0`;
+        database.query(query,[username],(error,result)=>{
+            if(error){
+                reject (error);
+                return;
+            }
+            if(result.length===0){
+                reject("Invalid username");
+                return;
+            }
+            resolve(result);
+        });
+    });
+   }catch(error){
+    throw error;
+   }
+}
+
+const addevent = async (fields)=>{
+    try{
+        return new Promise((resolve,reject)=>{
+            const query = 'CALL EventManager(?,?,?,?)'; 
+            database.query(query,fields,(error,data)=>{
+                if(error){
+                    reject(error);
+                    return;
+                }
+                if(data.length===0){
+                    reject(error);
+                    return;
+                }
+                resolve(data);
+            });
+        });
+    }catch(error){
+        throw error;
+    }
+}
+export default {
+    Menu,
+    EventModel,
+    superuser,
+    addevent
+};
